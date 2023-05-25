157a224f5d5510a6f6d8a9958520ea8aa298a819
Santyew <santiagoruiz5999@gmail.com>
2023-05-25 20:58:39 +0100

    Introduccion al c#

---
+++ ConsolaIntroducción/ConsolaIntroducción/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsolaIntroducción
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Aloha presidentes");
+            Console.ReadKey();
+        }
+    }
+}
